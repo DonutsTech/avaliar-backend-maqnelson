d206e7ef4f263fcdf609700025a09c214ab7cbc7
MilenaCLeme <milenaleme4@hotmail.com>
2026-02-25 11:14:27 -0300

    Add ModelModel with CRUD operations and error handling

---
+++ src/models/model/index.ts
@@ -0,0 +1,47 @@
+import { handlePrismaError } from "../../error";
+import type { Prisma } from "../../generated/prisma/client";
+import { prisma } from "../../prisma";
+
+class ModelModel {
+  async create(data: Prisma.ModelCreateInput) {
+    try {
+      return await prisma.model.create({ data });
+    } catch (error) {
+      handlePrismaError(error);
+    }
+  }
+
+  async findAll() {
+    try {
+      return await prisma.model.findMany();
+    } catch (error) {
+      handlePrismaError(error);
+    }
+  }
+
+  async findById(id: string) {
+    try {
+      return await prisma.model.findUnique({ where: { ID: id } });
+    } catch (error) {
+      handlePrismaError(error);
+    }
+  }
+
+  async update(id: string, data: Prisma.ModelUpdateInput) {
+    try {
+      return await prisma.model.update({ where: { ID: id }, data });
+    } catch (error) {
+      handlePrismaError(error);
+    }
+  }
+
+  async delete(id: string) {
+    try {
+      return await prisma.model.delete({ where: { ID: id } });
+    } catch (error) {
+      handlePrismaError(error);
+    }
+  }
+}
+
+export const modelModel = new ModelModel();
