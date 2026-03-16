6b1f4483e3d54a1c3ea1e2d92920d53a81de3238
MilenaCLeme <milenaleme4@hotmail.com>
2026-03-16 09:33:52 -0300

    Add MarkWheelsController with create and findAll methods

---
+++ src/controllers/markWheels/index.ts
@@ -0,0 +1,27 @@
+import type { NextFunction, Request, Response } from "express";
+import type { CreateMarkWheelsDto } from "../../@types/interface/createMarkWheels.dto";
+import { markWheelsService } from "../../services/markWheels";
+
+class MarkWheelsController {
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const body: CreateMarkWheelsDto = request.body;
+      const create = await markWheelsService.create(body);
+      return response.status(201).json(create);
+    } catch (error) {
+      next(error);
+
+    }
+  }
+
+  async findAll(_request: Request, response: Response, next: NextFunction) {
+    try {
+      const findAll = await markWheelsService.findAll();
+      return response.status(200).json(findAll);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export const markWheelsController = new MarkWheelsController();
