65d02e5acaaffbd8864098d4f4c26e0b03075d6f
MilenaCLeme <milenaleme4@hotmail.com>
2026-03-16 09:34:55 -0300

    Add MarkWheels schema to schemas index

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,10 +1,12 @@
 import type { ObjectSchema } from "joi";
 import createCheckin from "./checkin/create";
 import createUser from "./user/create";
+import createMarkWheels from "./markWheels";
 
 const schemas: { [key: string]: ObjectSchema } = {
   "user/create": createUser,
   "checkin/create": createCheckin,
+  "markWheels/create": createMarkWheels,
 };
 
 export default schemas;
