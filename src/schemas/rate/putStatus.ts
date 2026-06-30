d63c865b9647f17f7141bb165b5d84cc95932e9d
MilenaCLeme <milenaleme4@hotmail.com>
2026-06-29 21:58:50 -0300

    feat: update putFinance and putStatus schemas to allow empty strings for optional fields

--- src/schemas/rate/putStatus.ts
+++ src/schemas/rate/putStatus.ts
@@ -2,9 +2,9 @@ import Joi from 'joi';
 
 const putStatus = Joi.object({
   STATUS: Joi.string().max(250).optional(),
-  ACCEPTED: Joi.string().max(250).optional(),
-  APPROVED: Joi.string().max(250).optional(),
-  MESSAGE: Joi.string().max(250).optional(),
+  ACCEPTED: Joi.string().allow('').max(250).optional(),
+  APPROVED: Joi.string().allow('').max(250).optional(),
+  MESSAGE: Joi.string().allow('').max(250).optional(),
 });
 
 export default putStatus;
